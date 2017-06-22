07355b0a04717010bbad799a1ac24ba396cdc1f3
lcgkiller <lcgkiller@gmail.com>
2017-06-22 19:33:10 +0900

    메인 페이지 프로토타입

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^post/$', include('post.urls'))
+    url(r'^post/', include('post.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
